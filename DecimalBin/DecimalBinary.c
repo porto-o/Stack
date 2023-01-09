acdadf67b09ee9a5080c1b251f0345556cf34093
porto-o <ismael.porto2003@gmail.com>
2023-01-08 21:59:54 -0600

    Se agrego la funcion para convertir de decimal a binario en recursivo

--- DecimalBin/DecimalBinary.c
+++ DecimalBin/DecimalBinary.c
@@ -2,18 +2,28 @@
 #include "Pila.h"
 
 Pila convertirBinario(Elem);
+Pila convertirBinarioRecursivo(Elem, Pila);
 void ImprimirBinario(Pila);
 
 int main() {
     printf("Ingresa el numero a convertir: ");
     Elem number = LeerElem();
 
-    ImprimirBinario(convertirBinario(number));
+    Pila s = empty();
+
+    ImprimirBinario(convertirBinarioRecursivo(number,s));
     return 0;
 }
 
 Pila convertirBinario(Elem number)
 {
+    /**
+     * Dividimos el numero entre 2
+     * el residuo solo será 1 o 0 y lo meteremos a la pila
+     * cuando el número sea menor a 1, pararemos
+     * después imprimiremos la pila de arriba hacia abajo
+     * 
+    */
     Pila s = empty();
 
     while (number >= 1)
@@ -24,6 +34,20 @@ Pila convertirBinario(Elem number)
         s = push(residuo,s);
     }
 
+    return s;
+}
+
+Pila convertirBinarioRecursivo(Elem number,Pila s)
+{
+    if(!number < 1)
+    {
+        int residuo = number % 2;
+        number = number / 2;
+
+        s = push(residuo, s);
+        return convertirBinarioRecursivo(number,s); // OJOOOOOOOOOOOOOOOOOOOOO en el return, no olvidar :))))))
+    }
+   
     return s;
 }
 
